555f613cd2e50aa394c7c3e375881a348e6615ea
Desor Guishny <desorguishny@gmail.com>
2023-01-25 14:18:44 -0500

    Add splash page styles

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,6 +1,7 @@
-import react from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import astronaut from '../images/astronaut.png';
+import '../button.css';
 
 const Splash = () => {
   return (
